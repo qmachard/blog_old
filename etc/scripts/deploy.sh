1c6a6ed29de03fb164f38cf16d3da28e97628ab1
Quentin Machard <quentin.machard@gmail.com>
2018-06-14 20:33:56 +0200

    Correctly remove folders

--- etc/scripts/deploy.sh
+++ etc/scripts/deploy.sh
@@ -3,11 +3,11 @@
 if  [[ $TRAVIS_PULL_REQUEST = "false" ]]
 then
     ncftp -u "$USERNAME" -p "$PASSWORD" "$HOST"<<EOF
-    rm -rf /public_html/blog
-    mkdir /public_html/blog
+    rm -rf public_html/blog
+    mkdir public_html/blog
     quit
 EOF
 
     cd dist || exit
-    ncftpput -R -v -u "$USERNAME" -p "$PASSWORD" "$HOST" /public_html/blog .
+    ncftpput -R -v -u "$USERNAME" -p "$PASSWORD" "$HOST" public_html/blog .
 fi
