a97c0e40da102dd5bc3e1d7b6f527e926a80543e
Quentin Machard <quentin.machard@gmail.com>
2018-06-14 20:21:41 +0200

    Add deploy script

---
+++ etc/scripts/deploy.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+if  [[ $TRAVIS_PULL_REQUEST = "false" ]]
+then
+    ncftp -u "$USERNAME" -p "$PASSWORD" "$HOST"<<EOF
+    rm -rf /public_html/blog
+    mkdir /public_html/blog
+    quit
+EOF
+
+    cd dist || exit
+    ncftpput -R -v -u "$USERNAME" -p "$PASSWORD" "$HOST" /public_html/blog .
+fi
